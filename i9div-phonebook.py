db34c5823a4c67952e021faf1269ea9bdc0d8206
lordtemplar <lordtemplar0@gmail.com>
2024-09-21 08:59:25 +0700

    Update i9div-phonebook.py

--- i9div-phonebook.py
+++ i9div-phonebook.py
@@ -3,6 +3,9 @@ import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
 
+# Set up the page layout and update browser title
+st.set_page_config(page_title="ทำเนียบนายทหาร จปร. ค่ายสุรสีห์", layout="wide")
+
 # JavaScript to detect if the app is running in WebView or standalone mode and redirect to browser
 st.markdown("""
     <script>
@@ -12,9 +15,6 @@ st.markdown("""
     }
     </script>
 """, unsafe_allow_html=True)
-
-# Set up the page layout and update browser title
-st.set_page_config(page_title="ทำเนียบนายทหาร จปร. ค่ายสุรสีห์", layout="wide")
 
 # Authenticate and connect to Google Sheets
 def authenticate_google_sheets():
